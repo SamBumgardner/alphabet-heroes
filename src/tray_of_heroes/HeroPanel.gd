cef8479503c07681524e4cfa1a9cef2f83ed5733
Noah Bumgardner <noah@playgamecafe.com>
2024-04-13 16:21:51 -0500

    Populate Hero Texture from Upper Level

--- src/tray_of_heroes/HeroPanel.gd
+++ src/tray_of_heroes/HeroPanel.gd
@@ -2,8 +2,13 @@ extends PanelContainer
 
 @export var job_to_display : Hero.Job
 
+@export var portrait_to_display : CompressedTexture2D
+
 @onready var label_of_job : Label = (
 	$MarginContainer/HBoxContainer/HeroStatistics/LabelOfJob
+)
+@onready var portrait : TextureRect = (
+	$MarginContainer/HBoxContainer/HeroPortrait
 )
 
 var hero_class_name : String
@@ -13,6 +18,8 @@ func _ready():
 	print(job_to_display)
 	print(hero_class_name)
 	label_of_job.text = _build_hero_class_name_label()
+
+	portrait.texture = portrait_to_display
 
 func _build_hero_class_name_label() -> String:
 	return (
