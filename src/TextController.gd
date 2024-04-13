77e32898a2ad8dc7984c923c9af5cbef8b76036a
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-04-13 00:58:50 -0500

    TextController cleanup for reduced log noise

--- src/TextController.gd
+++ src/TextController.gd
@@ -17,16 +17,15 @@ func _unhandled_key_input(event:InputEvent):
 	if event.is_pressed():
 		var event_text = event.as_text()
 		if event_text == "Backspace":
-			print("Backspace key was pressed!")
-			try_pop_letter()
+			try_backspace_letter()
+			viewport.set_input_as_handled()
 		elif not event.is_echo():
 			if event_text.length() == 1 and alpha_regex.search(event_text):
 				try_append_letter(event_text)
-				print(current_word)
-			if event_text == "Enter":
-				print("Enter key was pressed!")
+				viewport.set_input_as_handled()
+			elif event_text == "Enter":
 				send_word()
-	viewport.set_input_as_handled()
+				viewport.set_input_as_handled()
 
 func try_append_letter(char_to_append : String) -> String:
 	if current_word.length() < max_letters:
@@ -35,10 +34,10 @@ func try_append_letter(char_to_append : String) -> String:
 
 func send_word():
 	if TextValidator.is_valid(current_word):
-		print("sending word %s to battle" % current_word)
+		print("sending word %s to battle!" % current_word)
 		current_word = ""
 	else:
-		print("tried to send, but %s is not a valid word" % current_word)
+		print("tried to start the battle, but %s is not a valid word" % current_word)
 
-func try_pop_letter():
+func try_backspace_letter():
 	current_word = current_word.substr(0, current_word.length() - 1)
