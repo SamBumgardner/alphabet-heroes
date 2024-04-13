9cf5ee96167ad82e647620f41929e61af7f13e26
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-04-13 12:31:34 -0500

    clean up party demo display

--- demos/party-controller/SimpleCombatPreviewVisualizer.gd
+++ demos/party-controller/SimpleCombatPreviewVisualizer.gd
@@ -5,4 +5,4 @@ func _on_party_changed(new_party:Party):
 	for i in range(new_party.combat_totals.size()):
 		result += "  %s: %s\n" % [CombatValues.Values.keys()[i], new_party.combat_totals[i]]
 	text = result
-	modulate = Color.from_hsv(randf(), randf(), randf())
+	#modulate = Color.from_hsv(randf(), randf(), randf())
--- demos/party-controller/SimplePartyVisualizer.gd
+++ demos/party-controller/SimplePartyVisualizer.gd
@@ -2,4 +2,4 @@ extends Label
 
 func _on_party_changed(new_party:Party):
 	text = new_party.to_string()
-	modulate = Color.from_hsv(randf(), randf(), randf())
+	#modulate = Color.from_hsv(randf(), randf(), randf())
