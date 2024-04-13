0fff9edc2201865acef75606b033a06d81bd6d25
Noah Bumgardner <noah@playgamecafe.com>
2024-04-13 09:53:22 -0500

    Move Folders into src and assets, Works

--- src/back_to_start_menu_button/BackToStartMenuButton.gd
+++ src/back_to_start_menu_button/BackToStartMenuButton.gd
@@ -3,4 +3,4 @@ extends Control
 
 func _on_exit_button_pressed() -> void:
 	Input.set_custom_mouse_cursor(null)
-	get_tree().change_scene_to_file("res://start_menu/StartMenu.tscn")
+	get_tree().change_scene_to_file("res://src/start_menu/StartMenu.tscn")
