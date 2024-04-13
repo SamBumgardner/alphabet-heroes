b2700758fa5c16a15bc48e3d329b9d44ff0a5284
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-04-13 12:06:05 -0500

    squash bugs in party-figuring-out

--- src/heroes/Hero.gd
+++ src/heroes/Hero.gd
@@ -4,16 +4,19 @@ var letter : String
 var job : Job
 
 func _init(letter_in:String, job_in:Job):
-    letter = letter_in
-    job = job_in
+	letter = letter_in
+	job = job_in
 
 func _to_string():
-    return "letter: %s, class: %s" % [letter, Hero.Job.keys()[job]]
+	return "letter: %s, class: %s" % [letter, Hero.Job.keys()[job]]
 
 enum Job {
-    WARRIOR = 0,
-    KNIGHT = 1,
-    MAGE = 2,
-    PRIEST = 3,
-    PEASANT = 4,
+	WARRIOR = 0,
+	KNIGHT = 1,
+	MAGE = 2,
+	PRIEST = 3,
+	PEASANT = 4,
 }
+
+static func random_job() -> Job:
+	return (randi() % Job.size() - 1) as Job
--- src/heroes/HeroRepository.gd
+++ src/heroes/HeroRepository.gd
@@ -1,37 +1,62 @@
 class_name HeroRepository extends Node
 
+const LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const GENERATE_BATCH_SIZE = 8
+
 signal hero_repository_contents_changed(current_repository)
 
 # Array of Dictionaries - one dict for each non-peasant hero job
 var _repository : Array
+var _available_letters : Dictionary
 
 func _init():
-    _repository = Array()
-    _repository.resize(Hero.Job.size() - 1) # exclude the peasants
-    _repository.fill({})
+	_repository = Array()
+	for i in range(Hero.Job.size() - 1): # exclude the peasants
+		_repository.append({})
+	_available_letters = {}
+	for letter in LETTERS:
+		_available_letters[letter] = true
 
 func add(heroes:Array):
-    for hero : Hero in heroes:
-        _repository[hero.job][hero.letter] = true
-    hero_repository_contents_changed.emit(_repository)
+	for hero : Hero in heroes:
+		_repository[hero.job][hero.letter] = true
+		_available_letters.erase(hero.letter)
+	hero_repository_contents_changed.emit(_repository)
 
 func remove(letters:String):
-    for letter in letters:
-        for dict : Dictionary in _repository:
-            dict.erase(letter)
-    hero_repository_contents_changed.emit(_repository)
+	for letter in letters:
+		for dict : Dictionary in _repository:
+			dict.erase(letter)
+			_available_letters[letter] = true
+	hero_repository_contents_changed.emit(_repository)
 
 func get_heroes(letters:String) -> Array:
-    var result = []
-    for letter in letters:
-        var hero : Hero = null
-        for dict : Dictionary in _repository:
-            if dict.find_key(letter):
-                hero = dict[letter]
-                break;
-        if hero == null:
-            hero = Hero.new(letter, Hero.Job.PEASANT)
-        result.append(hero)
-    
-    return result
-    
+	var result = []
+	var processed_letters = ""
+	for letter in letters:
+		var hero : Hero = null
+		print(_repository)
+		if not processed_letters.contains(letter):
+			for job in range(_repository.size()):
+				var dict = _repository[job]
+				if dict.has(letter):
+					hero = Hero.new(letter, job)
+					break;
+		if hero == null:
+			hero = Hero.new(letter, Hero.Job.PEASANT)
+		result.append(hero)
+		processed_letters += letter
+	
+	return result
+
+func generate_heroes():
+	var new_heroes = []
+	for i in range(min(GENERATE_BATCH_SIZE, _available_letters.size())):
+		var hero = Hero.new(
+			_available_letters.keys().pick_random(), 
+			Hero.random_job()
+		)
+		_available_letters.erase(hero.letter)
+		new_heroes.append(hero)
+	
+	add(new_heroes)
