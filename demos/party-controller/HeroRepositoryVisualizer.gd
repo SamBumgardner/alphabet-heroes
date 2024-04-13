5469cb7cb84578709562e6b63015cd5774c83d70
Noah Bumgardner <noah@playgamecafe.com>
2024-04-13 16:13:04 -0500

    Control Label from Panel Property

--- demos/party-controller/HeroRepositoryVisualizer.gd
+++ demos/party-controller/HeroRepositoryVisualizer.gd
@@ -2,11 +2,14 @@ extends Label
 
 @export var job_to_display : Hero.Job
 
+var hero_class_name
+
 func _ready():
+	hero_class_name = Hero.Job.keys()[job_to_display]
 	_on_hero_repository_contents_changed([{},{},{},{}])
 
 func _on_hero_repository_contents_changed(current_repository):
 	var letter_list = current_repository[job_to_display].keys()
-	text = "%s:\n" % Hero.Job.keys()[job_to_display]
+	text = "%s:\n" % hero_class_name
 	for letter in letter_list:
 		text += "%s, " % letter
---
+++ src/tray_of_heroes/HeroPanel.gd
@@ -0,0 +1,22 @@
+extends PanelContainer
+
+@export var job_to_display : Hero.Job
+
+@onready var label_of_job : Label = (
+	$MarginContainer/HBoxContainer/HeroStatistics/LabelOfJob
+)
+
+var hero_class_name : String
+
+func _ready():
+	hero_class_name = Hero.Job.keys()[job_to_display]
+	print(job_to_display)
+	print(hero_class_name)
+	label_of_job.text = _build_hero_class_name_label()
+	pass # Replace with function body.
+
+func _build_hero_class_name_label() -> String:
+	return (
+		hero_class_name.to_pascal_case()
+		+ ":"
+	)
