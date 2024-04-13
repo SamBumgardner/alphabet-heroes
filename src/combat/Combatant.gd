3007232562724e48da79cf2e79298b7d042d6e2a
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-04-13 15:48:26 -0500

    implement shared player/enemy functionality

---
+++ src/combat/Combatant.gd
@@ -0,0 +1,43 @@
+class_name Combatant extends Node
+
+signal hurt(previous_health:int, new_health:int)
+signal healed(previous_health:int, new_health:int)
+signal block_increased(previous_block:int, new_block:int)
+signal block_decreased(previous_block:int, new_block:int)
+
+@export var max_health : int = 50
+@export var current_health : int = 50
+@export var current_block : int = 0
+
+# can emit block_decreased, hurt signals
+func apply_damage(damage:int, is_magic:bool):
+	if damage <= 0:
+		return
+
+	var unmitigated_damage = damage
+	if current_block > 0 and not is_magic:
+		var old_block = current_block
+		current_block -= min(unmitigated_damage, old_block)
+		unmitigated_damage -= old_block
+		block_decreased.emit(old_block, current_block)
+	
+	if unmitigated_damage > 0:
+		var old_health = current_health
+		current_health -= unmitigated_damage
+		hurt.emit(old_health, current_health)
+
+func add_block(additional_block:int):
+	if additional_block <= 0:
+		return
+	
+	var old_block = current_block
+	current_block += additional_block
+	block_increased.emit(old_block, current_block)
+
+func apply_healing(healing:int):
+	if healing <= 0:
+		return
+	
+	var old_health = current_health
+	current_health += healing
+	healed.emit(old_health, current_health)
