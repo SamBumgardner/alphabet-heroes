7c569a79359ac833027adc949c96dddcfe179fbd
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-04-14 17:43:08 -0500

    Make heroes charge the enemy

--- src/display/summoned_heroes/AnimatedHeroDisplayer.gd
+++ src/display/summoned_heroes/AnimatedHeroDisplayer.gd
@@ -1,5 +1,7 @@
 class_name AnimatedHeroDisplayer extends Node
 
+@export var charge_destination : Vector2 = Vector2(0, 0)
+
 @onready var animatable_heroes : Array = get_children()
 
 var _current_party : Party
@@ -23,7 +25,23 @@ func _on_summon_hero_positions(summon_positions:Array):
 			animatable_hero.hide()
 
 func _on_player_windup(duration:float):
-	pass # set up tweening logic here
+	var summon_animation_duration = .34
+	var remaining_duration = duration - summon_animation_duration
+	var fade_out_duration = .25
+	
+	for summoned_hero in animatable_heroes:
+		var charge_tween = create_tween()
+		charge_tween.tween_interval(summon_animation_duration)
+		charge_tween.tween_callback(summoned_hero.play.bind("run"))
+		charge_tween.set_ease(Tween.EASE_IN)
+		charge_tween.set_trans(Tween.TRANS_CUBIC)
+		charge_tween.tween_property(
+			summoned_hero, 
+			"global_position", 
+			charge_destination, 
+			remaining_duration - fade_out_duration
+		)
+		charge_tween.tween_property(summoned_hero, "modulate", Color.TRANSPARENT, fade_out_duration)
 
 func _on_player_impact(duration:float):
 	for node in animatable_heroes:
