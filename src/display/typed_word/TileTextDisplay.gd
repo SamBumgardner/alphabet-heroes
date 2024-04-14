0a683405acabbebceaea331ca9f2e5f78e41dfc7
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-04-14 13:59:03 -0500

    add shake on invalid word

--- src/display/typed_word/TileTextDisplay.gd
+++ src/display/typed_word/TileTextDisplay.gd
@@ -2,12 +2,27 @@ class_name TileTextDisplay extends HBoxContainer
 
 @onready var letter_tiles : Array = get_children()
 
+var text_shake_tween : Tween
+var start_position : Vector2
+
+var sides = [
+	SIDE_BOTTOM,
+	SIDE_LEFT,
+	SIDE_RIGHT,
+	SIDE_TOP
+]
+var saved_anchors = [
+	0.0,
+	0.0,
+	0.0,
+	0.0
+]
+
 func _ready():
 	_set_word("")
+	_save_anchors()
 
 func _set_word(new_word:String):
-	modulate = Color.WHITE
-	
 	var upper_cased_word = new_word.to_upper()
 	var i = 0
 	for tile : LetterTile in letter_tiles:
@@ -15,9 +30,32 @@ func _set_word(new_word:String):
 			tile.set_letter(upper_cased_word[i])
 		else:
 			tile.set_letter("")
+		tile.label.modulate = Color.WHITE
 		i += 1
 
 # Color the denied word red for user feedback.
-func _color_denied_text() -> void:
-	if modulate != Color.RED:
-		modulate = Color.RED
+func _color_denied_text(_word:String):
+	for tile : LetterTile in letter_tiles:
+		tile.label.modulate = Color.RED
+	_reset_shake_tween()
+
+func _reset_shake_tween():
+	if text_shake_tween != null and text_shake_tween.is_valid():
+		text_shake_tween.stop()
+	start_position = position
+	text_shake_tween = create_tween()
+	text_shake_tween.tween_method(_shake, 0, 0, .1)
+	text_shake_tween.tween_callback(_reset_anchors)
+
+func _shake(_value : float):
+	position = start_position + (Vector2.ONE.rotated(randf_range(0, 6.29)) * 2)
+
+func _save_anchors():
+	saved_anchors[0] = anchor_bottom
+	saved_anchors[1] = anchor_left
+	saved_anchors[2] = anchor_right
+	saved_anchors[3] = anchor_top
+
+func _reset_anchors():
+	for i in range(sides.size()):
+		set_anchor_and_offset(sides[i], saved_anchors[i], 0, true) 
