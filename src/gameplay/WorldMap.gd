81a3a44be488586e183bb1431668f813a7a472a9
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-04-14 19:56:58 -0500

    removed demo functionality from world map

--- src/gameplay/WorldMap.gd
+++ src/gameplay/WorldMap.gd
@@ -1,20 +1,8 @@
 class_name WorldMap extends TextureRect
 
-const CAPSTONE_HILLS_POS = Vector2(-3509, -2396)
-const HEARTFELL_WOODS_POS = Vector2(-2435, -1658)
-
 const TRAVEL_TIME = 5
 
 var travel_tween : Tween
-
-func _unhandled_key_input(event):
-	if event.is_pressed() and not event.is_echo():
-		if event.as_text() == "F1":
-			_set_new_travel_tween(CAPSTONE_HILLS_POS)
-			get_viewport().set_input_as_handled()
-		if event.as_text() == "F2":
-			_set_new_travel_tween(HEARTFELL_WOODS_POS)
-			get_viewport().set_input_as_handled()
 
 func _set_new_travel_tween(target_position:Vector2):
 	if travel_tween != null and travel_tween.is_valid():
