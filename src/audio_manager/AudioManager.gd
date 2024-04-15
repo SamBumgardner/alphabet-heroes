619a9c2cae4769babbf59d725fe16fca73615130
Noah Bumgardner <noah@playgamecafe.com>
2024-04-14 21:21:57 -0500

    Hook Up Button Sounds to Exit Button

--- src/audio_manager/AudioManager.gd
+++ src/audio_manager/AudioManager.gd
@@ -42,6 +42,9 @@ func _on_start_menu_ready():
 func _on_credits_button_mouse_entered():
 	SoundManager.play_sound(sfx_button_hover)
 
+func _on_exit_button_mouse_entered():
+	SoundManager.play_sound(sfx_button_hover)
+
 func _on_quit_button_mouse_entered():
 	SoundManager.play_sound(sfx_button_hover)
 
@@ -53,6 +56,9 @@ func _on_start_button_mouse_entered():
 #region Button press
 
 func _on_credits_button_pressed():
+	SoundManager.play_sound(sfx_button_click)
+
+func _on_exit_button_pressed():
 	SoundManager.play_sound(sfx_button_click)
 
 #endregion Button press
