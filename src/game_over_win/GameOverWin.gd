6130a79f242dac7630c5fd03c3e4c11a9f83546d
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-05-11 13:36:44 -0500

    only display unlock pop-up if it's the player's first victory

--- src/game_over_win/GameOverWin.gd
+++ src/game_over_win/GameOverWin.gd
@@ -1,4 +1,12 @@
 extends Control
 
+var just_unlocked_hard_mode : bool = false 
+
 func _ready():
-	Database.set_player_beaten_normal(true)
+	if not Database.player_beaten_normal:
+		Database.set_player_beaten_normal(true)
+		just_unlocked_hard_mode = true
+
+func optional_unlock_notification():
+	if just_unlocked_hard_mode:
+		$UnlockNotification.pop_up()
